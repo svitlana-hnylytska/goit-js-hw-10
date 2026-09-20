fce80d8e62c4e834eae8ab2b10cb4732243db1e3
svitlana-hnylytska <81011990+svitlana-hnylytska@users.noreply.github.com>
2026-09-20 17:03:20 +0000

    Deploying to gh-pages from @ svitlana-hnylytska/goit-js-hw-10@2bf45e2bce194fd109fef0d7f16575acb5ad2ba4 🚀

---
+++ 2-snackbar.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{i as m}from"./assets/vendor-BbbuE1sJ.js";const t=document.querySelector(".form");t.addEventListener("submit",o=>{o.preventDefault();const s=Number(t.elements.delay.value),i=t.elements.state.value;new Promise((e,r)=>{setTimeout(()=>{i==="fulfilled"?e(s):r(s)},s)}).then(e=>{m.success({message:`✅ Fulfilled promise in ${e}ms`})}).catch(e=>{m.error({message:`❌ Rejected promise in ${e}ms`})})});
+//# sourceMappingURL=2-snackbar.js.map
